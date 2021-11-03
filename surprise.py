3a7aeb69b38177a35dbb282121de21d98e37b84e
Anushri-entrepreneur <82871385+Anushri-entrepreneur@users.noreply.github.com>
2021-11-03 22:16:22 +0530

    Create surprise.py

---
+++ surprise.py
@@ -0,0 +1,16 @@
+import streamlit as st
+st.title("DIWALI GIFT")
+st.image("ii.jpg",width=400)
+
+radd=st.sidebar.selectbox("choose",["CREATE WISH"])
+eng=st.sidebar.text_area("This time believe me and enter your wish")
+if st.sidebar.button("Done"):
+   st.success("success")
+st.write(eng)
+
+st.write("GOD HEARD YOUR WISH AND IT WILL HAPPEN FOR SURE")
+st.write("WISHING YOU AND SWAYAMPRABHA MAM A PROSPORUS DIWALI")
+st.write("GOD WILL ANSWER TRUE PRAYERS ")
+st.write("THE OUTPUT DIDNT SHOW FOR THE FIST TIME BECAUSE IT WAS ADDED TO GOD'S LIST OF ACCEPTING WISHES")
+st.write("YOU CAN SEE THE OUTPUT BECAUSE THIS TIME BECAUSE YOU WISHED TO SEE THAT'S WHY IT APPEARED")
+st.write("ONE SECOND HAPPY DIWALI TO YOU AND YOUR FAMILY SIR")
