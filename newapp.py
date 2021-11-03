7968ed57b66adcd14af459e368e6605b7946cf5c
Anushri-entrepreneur <82871385+Anushri-entrepreneur@users.noreply.github.com>
2021-11-03 18:01:57 +0530

    Add files via upload

---
+++ newapp.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.beta_set_page_config(page_title="APPS BY ANU")
+
+st.title("KARANE KE LIE SOOCHEE APP")
+
+rad=st.sidebar.radio("Navigation",["Soochee mein jodane","Adyatan soochee"])
+
+if rad=="Soochee mein jodane":
+   
+   a = st.checkbox("JAROORE"),
+   b=st.text_area("Enter list item")
+   d= st.checkbox("MAHATVAPOORN NAHIN")
+   e=st.text_area("Enter list ")
+   
+   if st.button("DONE"):
+       st.success("SUCCESS")
+
+if rad=="Adyatan soochee":
+    c=st.text_area("ADD UPDATES IF ANY")
+    if st.button("DONE"):
+       st.success("LIST UPDATED")
